57c368411fd9c355c43ab374674ec5f0e92d86c6
Eric Wasylishen <ewasylishen@gmail.com>
2016-12-07 17:11:40 -0700

    travis scripts: try building + packaging on mac and linux

--- travis-linux.sh
+++ travis-linux.sh
@@ -12,9 +12,9 @@ if [[ "daf03ed0006e41334f10ceeb3aa2d20c63aacd42" != $(sha1sum wxWidgets-3.1.0.7z
 7z x -o"wxWidgets" -y wxWidgets-3.1.0.7z > /dev/null
 cd wxWidgets || exit 1
 patch -p0 < ../patches/wxWidgets/*.patch || exit 1
-mkdir build-debug
-cd build-debug
-../configure --quiet --enable-debug --with-opengl --with-cxx=11 --with-gtk=2 --prefix=$(pwd)/install --disable-precomp-headers && make -j2 && make install
+mkdir build-release
+cd build-release
+../configure --quiet --disable-shared --with-opengl --with-cxx=11 --with-gtk=2 --prefix=$(pwd)/install --disable-precomp-headers && make -j2 && make install
 cd ..
 cd ..
 
@@ -22,8 +22,9 @@ cd ..
 
 mkdir build
 cd build
-cmake .. -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS=-Werror -DwxWidgets_PREFIX=$(pwd)/../wxWidgets/build-debug/install || exit 1
+cmake .. -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_CXX_FLAGS=-Werror -DwxWidgets_PREFIX=$(pwd)/../wxWidgets/build-release/install || exit 1
 ninja || exit 1
+cpack || exit 1
 
 # Run tests (wxgtk needs an X server running for the app to initialize)
 
